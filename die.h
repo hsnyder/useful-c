a512647388f02e7f105761e9279ca598c6af85c3
Harris M. Snyder <harris.snyder@gmail.com>
2022-05-10 11:58:58 -0400

    added die.h

---
+++ die.h
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <string.h>
+
+static void _Noreturn
+#if defined(__clang__) || defined(__GNUC__)
+__attribute__ ((format (printf, 1, 2)))
+#endif
+die(char *fmt, ...)
+{
+    int e = errno;
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    if (e!= 0) fprintf(stderr, " (errno %d: %s)", e, strerror(e));
+    fputc('\n', stderr);
+    exit(EXIT_FAILURE);
+}
